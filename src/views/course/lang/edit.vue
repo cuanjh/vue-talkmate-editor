8cdf4c44b3d9e24b4da0d8905a5c3178d455b523
talkmate <talkmate@hongfang-MacBook-Pro.local>
2020-01-19 11:53:50 +0800

    弹框

--- src/views/course/lang/edit.vue
+++ src/views/course/lang/edit.vue
@@ -1,7 +1,16 @@
 <template>
 <transition name="fade">
   <div class="edit-container" v-show="showEdit">
-    <div class="edit-content"></div>
+    <div class="edit-content">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="编码">
+          <el-input v-model="form.lan_code"></el-input>
+        </el-form-item>
+        <el-form-item label="编码">
+          <el-input v-model="form.title"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </transition>
 </template>
@@ -10,7 +19,17 @@
 export default {
   data () {
     return {
-      showEdit: false
+      showEdit: false,
+      form: {
+        desc: {}, // 描述
+        flag: [], // 小图标
+        is_hot: false, // 是否热门
+        is_show: false, // 是否上线
+        lan_code: '', // 语种的编码
+        list_order: 0, // 排序号
+        title: {}, // 名称
+        word_direction: '' // 从左到右还是相反
+      }
     }
   },
   mounted () {
