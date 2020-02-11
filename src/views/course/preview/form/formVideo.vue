2ad04c15763d7cd02214b14e583d99fcec680125
talkmate <talkmate@192.168.0.102>
2020-02-11 18:07:48 +0800

    视频

---
+++ src/views/course/preview/form/formVideo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="video-box">
+      <video></video>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  props: ['form'],
+  computed: {
+    ...mapState({
+      assetsDomain: state => state.course.assetsDomain
+    }),
+    videoUrl () {
+      let url = ''
+      if (this.form && this.form.video) {
+        url = this.assetsDomain + this.form.video
+      }
+      return url
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/course/preview/pro/modelVideo.vue
+++ src/views/course/preview/pro/modelVideo.vue
@@ -1,13 +1,45 @@
 <template>
-  <h1>视频</h1>
+  <div class="model-video">
+    <div class="model-content">
+      <form-video v-for="(form, index) in slideForms" :key="index" :form="form"/>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import FormVideoComp from '../form/formVideo'
 
+export default {
+  props: ['slideForms'],
+  created () {
+    this.$on('init', () => {
+      console.log('video init', this.slideForms)
+    })
+  },
+  components: {
+    'formVideo': FormVideoComp
+  },
+  computed: {
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.model-video {
+  width: 100%;
+  height: 100%;
+  background: pink;
+  .model-content {
+    width: 100%;
+    height: 100%;
+  }
+  .video-box {
+    width: 100%;
+    height: 100%;
+    video {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
